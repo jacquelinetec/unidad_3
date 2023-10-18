5b6fd6ddb37ff1bf0e11ea392fa81ae02aa37fa1
jacquelinetec <143621961+jacquelinetec@users.noreply.github.com>
2023-10-18 08:41:16 -0600

    Add files via upload

---
+++ Arreglos1.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arreglos.pkg1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Arreglos1 {
+
+  public static void main(String[] args) {
+     boolean[] banderas = {true, false, true, false, true,};
+     String[]nombres = {"dulce, arian, vale, luz, osmary" };
+     double[]calificaciones={5.7, 4.5, 5.6, 6.6,5.7, 4.5, 5.6, 6.6,9.3,10.0};
+        System.out.println("banderas - "+ nombres.length);
+        System.out.println("nombres-"+ calificaciones [2]);
+        System.out.println("calificacion en 0 "+calificaciones[0]);
+        System.out.println("calificacion en 1 "+calificaciones[1]);
+        System.out.println("calificacion en 2"+ calificaciones[2]);
+        System.out.println("calificacion en 3 "+calificaciones[3]);
+        System.out.println("calificacion en 4 "+calificaciones[4]);
+        System.out.println("calificacion en 5 "+calificaciones[5]);
+        System.out.println("calificacion en 6 "+calificaciones[6]);
+        System.out.println("calificacion en 7 "+calificaciones[7]);
+        System.out.println("calificacion en 8 "+calificaciones[8]);
+        System.out.println("calificacion en 9 "+calificaciones[9]);
+    }
+    
+}
